2cc9f61a326218e8d3f2247cada31e0bfeedfe00
gustavo <gustavorafaeldecarvalho@gmail.com>
2025-01-28 19:28:24 -0300

    style: add css to inputDefault & add: inputDefault to search input on peopleView

--- src/components/InputDefault.vue
+++ src/components/InputDefault.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
   type: { type: String, default: 'text' },
   label: { type: String, default: '' },
@@ -7,10 +9,16 @@ defineProps({
   mask: { type: String, default: null },
 })
 const model = defineModel()
+
+const isFocus = ref(false)
+
+function changeFocus(bool) {
+  isFocus.value = bool
+}
 </script>
 <template>
   <div>
-    <label for="name">{{ label }}</label>
+    <label :for="name" :class="{ active: model || isFocus }">{{ label }}</label>
     <input
       v-if="mask"
       :type="type"
@@ -18,7 +26,8 @@ const model = defineModel()
       :name="name"
       :id="name"
       v-mask="mask"
-      :placeholder="placeholder"
+      @focusin="changeFocus(true)"
+      @focusout="changeFocus(false)"
       required
     />
     <input
@@ -27,9 +36,69 @@ const model = defineModel()
       v-model="model"
       :name="name"
       :id="name"
-      :placeholder="placeholder"
+      @focusin="changeFocus(true)"
+      @focusout="changeFocus(false)"
       :maxlength="200"
       required
     />
   </div>
 </template>
+
+<style scoped>
+div {
+  position: relative;
+  margin-block: 1em;
+  margin-left: 1em;
+  padding: 0.25em;
+
+  border: 1px solid gray;
+  border-radius: 0.25em;
+
+  width: 14em;
+
+  @media (max-width: 1024px) {
+    width: 10em;
+  }
+  @media (max-width: 720px) {
+    width: 100%;
+  }
+}
+input {
+  font-size: 1em;
+  outline: none;
+  border: none;
+
+  padding: 0;
+  width: 14em;
+
+  @media (max-width: 1024px) {
+    width: 10em;
+  }
+  @media (max-width: 720px) {
+    width: 100%;
+  }
+  border-radius: 0.25em;
+}
+label {
+  position: absolute;
+  font-size: 1em;
+  left: 0.2em;
+  top: 0.25em;
+  color: var(--content-placeholder);
+  background-color: white;
+  padding: 0 0.25em;
+  transition: all 0.3s ease;
+  pointer-events: none;
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  max-width: 85%;
+  border-radius: 0.25em;
+}
+label.active {
+  font-size: 0.8em;
+  top: -0.75em;
+}
+</style>
--- src/components/people/PeopleView.vue
+++ src/components/people/PeopleView.vue
@@ -1,16 +1,26 @@
 <script setup>
 import PeopleList from '@/components/people/PeopleList.vue'
+import InputDefault from '../InputDefault.vue'
 import { useModalsStore } from '@/stores/modals'
+import { ref } from 'vue'
 
 const store = useModalsStore()
 const { peopleSwitch } = store
+
+const search = ref('')
 </script>
 <template>
   <div class="top-wrapper">
     Pessoas
     <button @click="peopleSwitch">adicionar</button>
   </div>
-  <input type="text" />
+  <InputDefault
+    v-model="search"
+    type="text"
+    label="Pesquisar"
+    name="search"
+    placeholder="Pesquisar"
+  />
   <PeopleList />
 </template>
 <style scoped></style>
